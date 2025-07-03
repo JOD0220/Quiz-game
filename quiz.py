bca38832bed2545719a678b5297337ea78967389
Shayaan <153784942+JOD0220@users.noreply.github.com>
2025-07-03 12:37:53 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,20 @@
+print("Welcometo my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing !="yes":
+    quit()
+
+print("Okay Lets Play :)")
+
+answer = input("Full form of CPU? ")
+if answer == "central processing unit":
+    print('Correct!')
+else:
+    print('Incorrect')
+   
+answer = input("Full form of RAM? ")
+if answer == "random access memory":
+    print('Correct!')
+else:
+    print('Incorrect')
